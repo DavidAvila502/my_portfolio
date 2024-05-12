446a71645f4ad4adf64c943f7db96e9b200c7f1c
David Avila <davidavila6002@gmail.com>
2024-05-11 20:40:33 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+const App = () => {
+   return (
+      // Main container
+      <div
+         className="min-h-[100vh]
+         bg-gradient-to-l from-[#0F2027] via-[#203A43] to-[#2C5364]
+   "
+      ></div>
+   );
+};
+
+export default App;
